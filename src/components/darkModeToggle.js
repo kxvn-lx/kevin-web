10505cb88d07d5c89b0803d312de9a2d9e839dad
Kevin Laminto <kevin.laminto@gmail.com>
2020-02-28 21:05:04 +0700

    Added toggle animation and page load transition

--- src/components/darkModeToggle.js
+++ src/components/darkModeToggle.js
@@ -17,6 +17,26 @@ const IconWrapper = styled.div`
     -moz-user-select: none; /* Firefox */
     -ms-user-select: none; /* IE10+/Edge */
     user-select: none; /* Standard */
+
+    &:hover {
+      position: relative;
+      animation: hover-animation 1s forwards ease;
+    }
+
+    &:hover:after {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 3px;
+      background-image: linear-gradient(45deg, black 50%, orange 50%);
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+      background-position: 100% 0;
+      position: absolute;
+      bottom: -0.25em;
+      right: 0;
+      animation: link-animation 1s ease-out forwards;
+    }
 `
 
 const DarkModeToggle = () => {
