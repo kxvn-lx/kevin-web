75542a668445656df6dff9b48d52b943e25f3715
Kevin Laminto <kevin.laminto@gmail.com>
2020-02-29 10:47:19 +0700

    .

--- src/components/main-alt.js
+++ src/components/main-alt.js
@@ -3,7 +3,7 @@ import React from "react"
 const Main_alt = props => {
     return (
         <div className="main container main-alt">
-            <h1>
+            <h1 className="coloured-text big-name">
                 {props.title}
             </h1>
             <h3>
