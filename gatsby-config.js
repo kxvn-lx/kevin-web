8b5da5ad50a9f775d289846db3919c6c0bcbbef8
Kevin Laminto <kevin.laminto@gmail.com>
2020-06-28 14:35:04 +1000

    Added site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `kevinweb.tech`,
+    description: `iOS Developer, interested in UI/UX design.`,
+    author: `Kevin Laminto <kevin.laminto@gmail.coM>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
